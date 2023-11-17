66c1e5902c2b3b7258f952965786151cc6e62750
lucas <parra.lucas@laposte.net>
2023-11-17 14:30:42 +0100

    routing sf

--- src/Controller/CoolStuffController.php
+++ src/Controller/CoolStuffController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-
+ 
 class CoolStuffController extends AbstractController
 {
     /**
@@ -18,4 +18,35 @@ class CoolStuffController extends AbstractController
             'path' => 'src/Controller/CoolStuffController.php',
         ]);
     }
+
+  /**
+ * @Route("/blog/{slug}", name="slug", requirements={"slug"="\S+"})
+ */
+
+    public function slug(string $slug): JsonResponse
+    {
+      return $this->json([
+      'message' => 'page blog'.$slug,
+      'path' => 'src/Controller/CoolStuffController.php',
+    ]);
+    }
+     /** 
+    *@Route("/blog/carte",name="carte")
+    */
+    public function carte(): JsonResponse
+    {
+      return $this->json("carte");
+    }
+      /** 
+    *@Route("/blog/{page}",name="page",  requirements={"page"="\d+"})
+    */
+    public function page(int $page): JsonResponse
+    {
+        return $this->json([
+            'message' => 'page '.$page,
+            'path' => 'src/Controller/CoolStuffController.php',
+        ]);
+
+    }
 }
+
