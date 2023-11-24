bdd3d1003799bfb785aecc70a0146a260e7a218f
lucas <parra.lucas@laposte.net>
2023-11-24 16:30:41 +0100

    crud

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -22,6 +22,11 @@ class Todo
      */
     private $name;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +40,18 @@ class Todo
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
